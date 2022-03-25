97e61bf3158eaf19f95114e3b6bde5e52b02baf0
nyxsr <sahrulramdan.75@gmail.com>
2022-03-25 13:11:28 +0700

    Pembuatan Tahap Awal Portal

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal SI-PAUD HI</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+
+    <h1 class="bigWelcome text-center">Selamat Datang di Portal SI PAUD-HI</h1>
+    <div class="portalLink ">
+        <a href="">
+            <img src="/img/icon/educationPortal.png" alt="">
+            <h3>Pendidikan</h3>
+        </a>
+        <a href="">
+            <img src="/img/icon/healthPortal.png" alt="">
+            <h3>Kesehatan</h3>
+        </a>
+        <a href="">
+            <img src="/img/icon/protectionPortal.png" alt="">
+            <h3>Perlindungan</h3>
+        </a>
+    </div>
+
+    <footer class="bg-light text-center text-lg-start">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright:
+            <a class="text-dark">NyxSR</a>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
